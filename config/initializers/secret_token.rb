1c16a81c26834ab4e64ee7a575b4c44d29b9912f
oleg <929528@gmail.com>
2013-10-18 09:59:27 +0300

    Создание репозитория

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PetroleumAccounting::Application.config.secret_key_base = 'a1718ed93520b740f3bac64ccef46c3233174e263a4fbe356a592b8e9a0c5f636a6424f1bdcf40ecafa3abcc5a70d7cd365592fa86f89162f326c498d61157e7'
